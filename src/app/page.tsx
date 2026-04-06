a1f0b6b292dacc53164824d0437399b0a8b259f8
Jatin Satare <jatinsatare@gmail.com>
2026-04-06 16:49:54 +0530

    add mobile first approach navbar to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main>
 
-        <h1>Resin Kalaakari</h1>
-
-
     </main>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,41 @@
+'use client'
+
 import Link from 'next/link'
 import styles from '@/components/Navbar/Navbar.module.css'
+import { useState } from 'react'
+import { FiMenu, FiShoppingCart } from "react-icons/fi";
+
 
 export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
     return (
-        <nav className={styles.nav}>
-            <Link href='/'>
+        // Navbar
+        <nav className={`${styles.nav} ${styles.links}`}>
+            {/* Left: Hamburger */}
+            <button onClick={() => setOpen(!open)} className={styles.menuBtn}>
+                <FiMenu size={20} />
+            </button>
+
+            {/* Center: Logo */}
+            <Link href='/' className={styles.logo}>
                 <h2>Resin Kalaakari</h2>
             </Link>
-            <div className={styles.links}>
-                <Link href='/products'>Products</Link>
-                <Link href='/cart'>Cart</Link>
+            {/* right: Links */}
+            <div className={`${styles.links} ${styles.cart}`}>
+                <Link href='/cart'>
+                    <FiShoppingCart size={20} />
+                </Link>
             </div>
+
+            {/* Mobile menu hamburger */}
+            {open &&
+                (<div className={styles.menu}>
+                    <Link href="/products">Products</Link>
+                    <Link href="/cart">Cart</Link>
+                </div>)
+            }
 
         </nav>
     )
