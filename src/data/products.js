469bd63a28e212a881f0ea211eb32eb6b490297e
Jatin Satare <jatinsatare@gmail.com>
2026-04-08 22:41:25 +0530

    update products data

--- src/data/products.js
+++ src/data/products.js
@@ -4,35 +4,41 @@ export const products = [
     name: "Resin Tray",
     price: 999,
     image: "/hero1.png",
+    description: "Beautiful handcrafted resin tray"
   },
   {
     id: 2,
     name: "Resin Coaster",
     price: 499,
     image: "/hero2.png",
+    description: "Elegant resin coaster set"
   },
   {
     id: 3,
     name: "Resin Tray",
     price: 999,
     image: "/hero1.png",
+    description: "Beautiful handcrafted resin tray"
   },
   {
     id: 4,
     name: "Resin Coaster",
     price: 499,
     image: "/hero2.png",
+    description: "Elegant resin coaster set"
   },
   {
     id: 5,
     name: "Resin Tray",
     price: 999,
     image: "/hero1.png",
+    description: "Beautiful handcrafted resin tray"
   },
   {
     id: 6,
     name: "Resin Coaster",
     price: 499,
     image: "/hero2.png",
+    description: "Elegant resin coaster set"
   },
 ];
