7a2e159428774fa71451273cf35f77fb40c853ff
Jatin Satare <jatinsatare@gmail.com>
2026-04-08 22:42:20 +0530

    add fonts & CartContext provider to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,17 @@
 import Navbar from '@/components/Navbar/Navbar'
 import './globals.css'
+import CartProvider from '@/context/CartContext'
+import { Inter, Playfair_Display } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-playfair',
+})
 
 export default function RootLayout({
   children,
@@ -8,9 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-        {children}
+      <body className={`${inter.variable} ${playfair.variable}`}>
+        <CartProvider>
+          <Navbar />
+          {children}
+        </CartProvider>
+
       </body>
     </html>
   );
