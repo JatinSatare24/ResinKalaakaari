4f1ed766e26ecd26fb5b08eb5025773ab1700e69
Jatin Satare <jatinsatare@gmail.com>
2026-04-09 11:53:22 +0530

    updated hamburger menu logic and style classes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,19 @@ import Link from 'next/link'
 import styles from '@/components/Navbar/Navbar.module.css'
 import { useState, useEffect } from 'react'
 import { FiMenu, FiShoppingCart } from "react-icons/fi";
+import { useContext } from 'react';
+import { CartContext } from '@/context/CartContext';
 
 
 export default function Navbar() {
 
     const [open, setOpen] = useState(false)
+
+    const { cart } = useContext(CartContext)!
+
+    const totalItems = cart.reduce((acc, item) => {
+        return acc + item.quantity
+    }, 0)
 
     // Lock scroll
 
@@ -40,27 +48,38 @@ export default function Navbar() {
             {/* Desktop Links */}
 
             <div className={styles.desktopLinks}>
-                <Link href="/products">Products</Link>
+                <Link href="/products" >Products</Link>
                 <Link href="/cart">Cart</Link>
             </div>
 
             {/* right: Cart */}
 
             <div className={`${styles.links} ${styles.cart}`}>
-                <Link href='/cart'>
-                    <FiShoppingCart size={20} />
+                <Link href="/cart" className={styles.cartLink}>
+                    <FiShoppingCart size={22} />
+                    {totalItems > 0 && (
+                        <span className={styles.badge}>{totalItems}</span>
+                    )}
                 </Link>
             </div>
 
             {/* Mobile menu hamburger */}
-            {open &&
-                (<div className={styles.menu}>
-                    <button onClick={() => setOpen(false)} className={styles.close}>✕</button>
-                    <Link href="/products">Products</Link>
-                    <Link href="/cart">Cart</Link>
-                </div>)
+            {
+                open && (
+                    <div className={styles.menu}>
+                        <div className={styles.menuHeader}>
+                            <span className={styles.menuTitle}>Menu</span>
+                            <button onClick={() => setOpen(false)} className={styles.close}>✕</button>
+                        </div>
+
+                        <nav className={styles.menuLinks}>
+                            <Link href="/products" onClick={() => setOpen(false)}>Products</Link>
+                            <Link href="/cart" onClick={() => setOpen(false)}>Cart</Link>
+                        </nav>
+                    </div>
+                )
             }
 
-        </nav>
+        </nav >
     )
 }
