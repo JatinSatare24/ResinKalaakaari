f71a387fbb9005d93123578bd35baafd2e3a7804
Jatin Satare <jatinsatare@gmail.com>
2026-04-09 11:51:56 +0530

    add CartContext, removeFromCart & updateQuantity logic to the CartCard

---
+++ src/components/CartCard/CartCard.tsx
@@ -0,0 +1,42 @@
+import styles from '@/components/CartCard/CartCard.module.css'
+import { CartContext } from "@/context/CartContext"
+import { useContext } from "react"
+
+export default function CartCard({ item }: any) {
+
+    const { removeFromCart, updateQuantity } = useContext(CartContext)!
+
+    return (
+        <div className={styles.card}>
+
+            <img 
+                src={item.image} 
+                alt={item.name} 
+                className={styles.image}
+            />
+
+            <div className={styles.content}>
+                <h2 className={styles.name}>{item.name}</h2>
+                <p className={styles.price}>₹{item.price}</p>
+
+                <div className={styles.controls}>
+                    <button onClick={() => updateQuantity(item.id, -1)}>-</button>
+                    <span>{item.quantity}</span>
+                    <button onClick={() => updateQuantity(item.id, +1)}>+</button>
+                </div>
+
+                <p className={styles.total}>
+                    ₹{item.price * item.quantity}
+                </p>
+            </div>
+
+            <button 
+                className={styles.remove}
+                onClick={() => removeFromCart(item.id)}
+            >
+                ✕
+            </button>
+
+        </div>
+    )
+}
