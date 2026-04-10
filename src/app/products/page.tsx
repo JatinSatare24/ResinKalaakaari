755a6ab9c1efc143a521d8c86c8dbc0d72294c4e
Jatin Satare <jatinsatare@gmail.com>
2026-04-10 20:26:37 +0530

    update page logic to fetch real data from supabase

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,26 @@
 import ProductCard from '@/components/ProductCard/ProductCard'
-import { products } from '@/data/products'
+// import { products } from '@/data/products'
 import styles from '@/components/ProductCard/ProductCard.module.css'
+import { supabase } from '@/lib/supabase'
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+
+    const { data, error } = await supabase
+        .from('products')
+        .select('*')
+
+    if (error) {
+        console.error('Error fetching products: ', error.message)
+        return <p>Failed to load products!</p>
+    }
+
+    const products = data ?? []
+
     return (
         <main>
             <h1>Products</h1>
             <div className={styles.productGrid}>
-                {products.map(product => (
+                {products?.map(product => (
                     <ProductCard key={product.id} product={product} />
                 ))}
             </div>
