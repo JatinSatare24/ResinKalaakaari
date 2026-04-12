30191a52c2edf6689bfe8a3484b8a3d9d0cc4edd
Jatin Satare <jatinsatare@gmail.com>
2026-04-12 20:19:45 +0530

    add featured products functionality with view all button in home page

---
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import styles from '@/components/FeaturedProducts/FeaturedProducts.module.css'
+import ProductCard from '../ProductCard/ProductCard';
+import { supabase } from '@/lib/supabase'
+import { useEffect, useState } from 'react';
+import { ArrowRight } from "lucide-react";
+import Link from 'next/link';
+
+type Product = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    image_url: string;
+    slug: string;
+};
+
+
+export default function FeaturedProducts() {
+
+    const [featuredProducts, setFeaturedProducts] = useState<Product[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const fetchFeaturedProducts = async () => {
+            const { data, error } = await supabase
+                .from('products')
+                .select('*')
+                .eq('is_featured', true)
+                .limit(4)
+                .order('created_at', { ascending: false });
+
+            if (error) {
+                console.error('Error fetching featured products: ', error.message)
+                setError('Failed to load featured products!')
+            } else {
+                setFeaturedProducts(data || [])
+            }
+
+            setLoading(false)
+
+        }
+
+        fetchFeaturedProducts()
+    }, [])
+
+    if (loading) {
+        return <p className={styles.message}>Loading featured products...</p>;
+    }
+
+    if (error) {
+        return <p className={styles.message}>{error}</p>;
+    }
+
+    if (featuredProducts.length === 0) {
+        return null;
+    }
+
+    return (
+        <section className={styles.featuredSection}>
+            <h2 className={styles.heading}>Featured Products</h2>
+
+            <div className={styles.grid}>
+                {featuredProducts.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+                <div className={styles.viewAllContainer}>
+                    <Link href="/products" className={styles.viewAllButton}>
+                        View All <ArrowRight size={18} className={styles.icon} />
+                    </Link>
+                </div>
+            </div>
+        </section>
+    )
+}
