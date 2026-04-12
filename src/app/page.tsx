0c62e3cd9e0e473639257e61c7bfc84813a9c8ff
Jatin Satare <jatinsatare@gmail.com>
2026-04-12 20:15:11 +0530

    add FeaturedProducts & shopByCategory components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
 import Hero from '@/components/Hero/Hero'
+import FeaturedProducts from '@/components/FeaturedProducts/FeaturedProducts'; 
+import ShopByCategory from '@/components/ShopByCategory/ShopByCategory';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <FeaturedProducts />
+      <ShopByCategory />
     </main>
   );
 }
