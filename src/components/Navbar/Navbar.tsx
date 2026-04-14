931fb172ef31d97c37895d2c07629682ee6d9867
Jatin Satare <jatinsatare@gmail.com>
2026-04-14 22:21:21 +0530

    add user icon & responsive to the navbar component across the app

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,9 +3,10 @@
 import Link from 'next/link'
 import styles from '@/components/Navbar/Navbar.module.css'
 import { useState, useEffect } from 'react'
-import { FiMenu, FiShoppingCart } from "react-icons/fi";
+import { FiMenu, FiShoppingCart, FiUser } from "react-icons/fi";
 import { useContext } from 'react';
 import { CartContext } from '@/context/CartContext';
+import Image from 'next/image';
 
 
 export default function Navbar() {
@@ -36,31 +37,50 @@ export default function Navbar() {
         // Navbar
         <nav className={`${styles.nav} ${styles.links}`}>
             {/* Left: Hamburger */}
-            <button onClick={() => setOpen(!open)} className={styles.menuBtn}>
-                <FiMenu size={20} />
-            </button>
+
+                <button onClick={() => setOpen(!open)} className={styles.menuBtn}>
+                    <FiMenu size={22} />
+                </button>
+   
+
 
             {/* Center: Logo */}
-            <Link href='/' className={styles.logo}>
-                <h2>Resin Kalaakari</h2>
-            </Link>
+        
+                <Link href='/' className={styles.logo}>
+                    <Image
+                        src='/Logo/logo.png'
+                        alt='Resin Kalaakaari logo'
+                        width={130}
+                        height={70}
+                        className={styles.image}
+                    />
+                </Link>
+           
+
 
             {/* Desktop Links */}
 
-            <div className={styles.desktopLinks}>
+            {/* <div className={styles.desktopLinks}>
                 <Link href="/products" >Products</Link>
                 <Link href="/cart">Cart</Link>
-            </div>
+            </div> */}
 
             {/* right: Cart */}
 
             <div className={`${styles.links} ${styles.cart}`}>
+
+                <Link href='/login' className={styles.cartLink}>
+                    <FiUser size={24} />
+                </Link>
+
                 <Link href="/cart" className={styles.cartLink}>
                     <FiShoppingCart size={22} />
                     {totalItems > 0 && (
                         <span className={styles.badge}>{totalItems}</span>
                     )}
                 </Link>
+
+
             </div>
 
             {/* Mobile menu hamburger */}
