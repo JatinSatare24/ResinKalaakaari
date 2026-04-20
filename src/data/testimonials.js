89a8dbb1fa88ab18d4423d47dd4caa9e50e10482
Jatin Satare <jatinsatare@gmail.com>
2026-04-20 22:12:53 +0530

    update testimonial & hero section arrays

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,37 +1,37 @@
 export const testimonials = [
   {
     id: 1,
-    image: '/Testimonials/ManaliKadamTestimonial.jpg',
+    image: '/Testimonials/ManaliKadamTestimonial.webp',
     alt: 'user testimonial'
   },
   {
     id: 2,
-    image: '/Testimonials/AnkithaShahiTestimonial.jpg',
+    image: '/Testimonials/AnkithaShahiTestimonial.webp',
     alt: 'user testimonial'
   },
   {
     id: 3,
-    image: '/Testimonials/AkashGuptaTestimonial.jpg',
+    image: '/Testimonials/AkashGuptaTestimonial.webp',
     alt: 'user testimonial'
   },
   {
     id: 4,
-    image: '/Testimonials/testimonial4.jpg',
+    image: '/Testimonials/testimonial4.webp',
     alt: 'user testimonial'
   },
   {
     id: 5,
-    image: '/Testimonials/testimonial5.jpg',
+    image: '/Testimonials/testimonial5.webp',
     alt: 'user testimonial'
   },
   {
     id: 6,
-    image: '/Testimonials/testimonial6.jpg',
+    image: '/Testimonials/testimonial6.webp',
     alt: 'user testimonial'
   },
   {
     id: 7,
-    image: '/Testimonials/testimonial8.jpg',
+    image: '/Testimonials/testimonial8.webp',
     alt: 'user testimonial'
   },
 ];
