df2d4ddc254d5f2323dad4a4ee5412196a708ad3
Jatin Satare <jatinsatare@gmail.com>
2026-04-21 23:25:35 +0530

    update Hero carousel image assets

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,18 +1,18 @@
 export const slides = [
-        {
-            image: "/Hero/hero1.webp",
-            title: "Handcrafted Resin Art",
-            subtitle: "Unique pieces made with creativity",
-        },
-        {
-            image: "/Hero/hero2.webp",
-            title: "Elegant Home Decor",
-            subtitle: "Add beauty to your space",
-        },
-         {
-            image: "/Hero/hero3.webp",
-            title: "Elegant frame",
-            subtitle: "Handmade resin frames",
-        },
-    ]
+    {
+        image: "/Hero/Hero1.webp",
+        title: "Handcrafted Resin Art",
+        subtitle: "Unique pieces made with creativity",
+    },
+    {
+        image: "/Hero/Hero2.webp",
+        title: "Elegant Home Decor",
+        subtitle: "Add beauty to your space",
+    },
+    {
+        image: "/Hero/Hero3.webp",
+        title: "Elegant frame",
+        subtitle: "Handmade resin frames",
+    },
+]
 
